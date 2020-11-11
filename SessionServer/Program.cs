37b7e52de7a10af2c58838dc5659085d85b0a7eb
rudty <rudtyz@gmail.com>
2020-11-11 21:57:51 +0900

    33

---
+++ SessionServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using SocketServer;
+using SocketServer.Net;
+using SocketServer.Net.IO;
+
+namespace SessionServer {
+    class Program {
+        static Task OnUserMessageListener(Session session, CPacket packetInputStream) {
+            Hello h = Hello.Parser.ParseFrom(packetInputStream.Buffer.ToArray());
+            
+
+            return Task.CompletedTask;
+        }
+        static void Main(string[] args) {
+            var s = new Server();
+            s.AddEventListener("hello", OnUserMessageListener);
+            s.ListenAndServe("0.0.0.0", 8080);
+        }
+    }
+}
